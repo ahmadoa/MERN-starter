7921bdd371a6809ac9be014483097012d51d560b
ahmad oa <ouladaouida@gmail.com>
2023-05-28 23:58:00 +0100

    created a user model & connected to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + "/server/.env" });
 
-app.listen(3001, () => {
-  console.log("server running");
+const UserModel = require("./models/Users");
+
+// connecting to db
+// listening to port
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    app.listen(3001, () => {
+      console.log("server running");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
